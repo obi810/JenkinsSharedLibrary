0d1ce0e0591bebb01b734724075068c1e70d1aee
obi810 <142725504+obi810@users.noreply.github.com>
2023-09-29 15:30:36 +1000

    Update cohort12.groovy

--- vars/cohort12.groovy
+++ vars/cohort12.groovy
@@ -1,16 +1,16 @@
 def call(string StageName){
 
-  if("${StageName}" == 'Build'){
+  if("${StageName}" == "Build"){
     echo "building package"
     sh "mvn package"
     echo "build success"
   }
-   if("${StageName}" == 'Test'){
+   if("${StageName}" == "Test"){
     echo "test with sonarqube"
     sh "mvn sonar:sonar"
     echo "test done"
   }
-   if("${StageName}" == 'Deploy'){
+   if("${StageName}" == "Deploy"){
     echo "deploying to nexus"
     sh "mvn deploy"
     echo "deployment successful"
