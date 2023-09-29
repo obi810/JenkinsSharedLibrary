9bd3cc3a0a3efcb35a1ef06ffcbe95471c81f555
obi810 <142725504+obi810@users.noreply.github.com>
2023-09-29 15:12:43 +1000

    Update cohort12.groovy

--- vars/cohort12.groovy
+++ vars/cohort12.groovy
@@ -1,13 +1,11 @@
 def call(string stagename){
 
   if("${stagename}" == 'Build'){
-    then
     echo "echo building package"
     sh "mvn package"
     echo "building done"
   }
   else if("${stagename}" == 'Test'){
-    then 
     echo "test package"
     sh "mvn package"
     echo "test done"
